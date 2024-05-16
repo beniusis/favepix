df618b568215cfde2d9a6ded710620ec3efd1490
beniusis <rokasbeniu@gmail.com>
2024-05-16 22:29:03 +0300

    chore: customized the import alias, added custom tailwind colors & animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        'drop-down-fade-in': {
+          '0%': { transform: 'translateY(-100%)', opacity: 0 },
+          '100%': { transform: 'translateY(0)', opacity: 1 }
+        }
+      },
+      animation: {
+        enter: 'drop-down-fade-in 300ms linear'
+      },
+      colors: {
+        text: '#10100f',
+        background: '#ebece9',
+        primary: '#464738',
+        secondary: '#b7baa0',
+        accent: '#8c9060'
+      }
+    }
   },
   plugins: []
 };
