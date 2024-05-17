2095a23b3b1d6f0e604741c48bc3b9d8e9b876bc
beniusis <rokasbeniu@gmail.com>
2024-05-17 21:02:42 +0300

    ci: deploy to github pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 import { Favorites, Home } from '../pages';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      Component: Home
+    },
+    {
+      path: '/favorites',
+      Component: Favorites
+    }
+  ],
   {
-    path: '/',
-    Component: Home
-  },
-  {
-    path: '/favorites',
-    Component: Favorites
+    basename: '/favepix/'
   }
-]);
+);
