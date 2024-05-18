e127239f89f7699bfc44dbd24e371c7c03109863
beniusis <rokasbeniu@gmail.com>
2024-05-18 20:12:16 +0300

    refactor: refactored router and its structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
 import ReactDOM from 'react-dom/client';
 import { StrictMode } from 'react';
 import { Toasts } from '@/components';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Favorites, Home, NotFound } from '@/pages';
 import './assets/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Toasts />
-    <RouterProvider router={router} />
+    <Router basename="/favepix/">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   </StrictMode>
 );
